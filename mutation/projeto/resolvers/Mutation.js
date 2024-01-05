d3a9d67c574eba2ad151a49dbb1e0ea13faad498
claralmora <clara.moura@cotabox.com.br>
2024-01-05 20:24:13 -0300

    edit(14): Adicionando mutation "novoUsuario"

---
+++ mutation/projeto/resolvers/Mutation.js
@@ -0,0 +1,22 @@
+const { usuarios, proximoId } = require('../data/db')
+
+module.exports = {
+    novoUsuario(_, args) {
+        const emailExistente = usuarios
+            .some(u => u.email === args.email)
+
+        if(emailExistente) {
+            throw new Error('E-mail cadastrado')
+        }
+        
+        const novo = {
+            id: proximoId(),
+            ...args,
+            perfil_id: 1,
+            status: 'ATIVO'
+        }
+
+        usuarios.push(novo)
+        return novo
+    }
+}
