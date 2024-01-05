3c94db3ac5160c19631b939183c7ce37a9f372f6
claralmora <clara.moura@cotabox.com.br>
2024-01-05 20:48:09 -0300

    edit(15): Arrumando uso da função "proximoId"

--- mutation/projeto/data/db.js
+++ mutation/projeto/data/db.js
@@ -9,21 +9,21 @@ const perfis = [
 ]
 
 const usuarios = [{
-    id: proximoId,
+    id: proximoId(),
     nome: 'João Silva',
     email: 'jsilva@zemail.com',
     idade: 29,
     perfil_id: 1,
     status: 'ATIVO'
 }, {
-    id: proximoId,
+    id: proximoId(),
     nome: 'Rafael Junior',
     email: 'rafajun@wemail.com',
     idade: 31,
     perfil_id: 2,
     status: 'INATIVO'
 }, {
-    id: proximoId,
+    id: proximoId(),
     nome: 'Daniela Smith',
     email: 'danismi@umail.com',
     idade: 24,
