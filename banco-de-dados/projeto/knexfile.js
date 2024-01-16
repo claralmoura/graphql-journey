cc5fad63ed772fa6e6fda39b1a68f3ce4f4ec4ed
claralmoura <clara.moura@cotabox.com.br>
2024-01-16 16:48:48 -0300

    edit(21): adicionando a configuração do projeto de migrations

---
+++ banco-de-dados/projeto/knexfile.js
@@ -0,0 +1,20 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+
+const { connection } = require('./.env');
+module.exports = {
+
+  client: 'mysql',
+  connection,
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations'
+  }
+
+};
